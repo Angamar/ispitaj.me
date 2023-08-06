34a8e63818de48c400aa1725f04460d85cd5ec2b
Aleksa Stojanovic <aleksa_95@msn.com>
2023-08-06 18:41:41 +0200

    fix: total number of question input field now saves preferance

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
                   onChange={(string, number) =>
                     options.setNumberOfQuestions(number)
                   }
-                  defaultValue={10}
+                  defaultValue={options.numberOfQuestions}
                   min={5}
                   max={100}
                 >
