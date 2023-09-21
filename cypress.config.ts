489f0037432eda52874bbdf7f7becdc5665c9d38
Aleksa Stojanovic <aleksa_95@msn.com>
2023-09-21 21:46:35 +0200

    test: skipping some tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    excludeSpecPattern: [
+      "e2e/1-getting-started/**",
+      "e2e/2-advanced-examples/**",
+    ],
   },
 });
